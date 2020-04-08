b74a93adc4bf34b79cb0c1afab91b9ef4f100314
jack <jack@jack.com>
2020-04-08 18:02:48 +0300

    mıntıka temizliği

--- assets/js/medium-parser.js
+++ assets/js/medium-parser.js
@@ -31,8 +31,6 @@ let gotFeed = response => {
             let $time = $("<span/>").addClass("g-color-gray-dark-v4 g-font-size-12").text(element.pubDate);
             let $itemContent = $("<p/>").addClass("font-medium").text(element.title);
             let $footer = $("<ul/>").addClass("list-inline d-sm-flex my-0 font-medium");
-            // let $claps = $("<li/>").addClass("list-inline-item g-mr-20 g-color-gray-dark-v4 g-color-primary--hover")
-            //                        .html('<i class="fa fa-thumbs-up g-pos-rel g-top-1 g-mr-3"></i>' + element.claps);
             let $readMore = $("<li/>").addClass("list-inline-item ml-auto")
                                       .append($("<a/>").addClass("u-link-v5 g-color-gray-dark-v4 g-color-primary--hover")
                                                        .attr("href", element.guid)
@@ -42,7 +40,6 @@ let gotFeed = response => {
             $header.append($user);
             $header.append($time);
 
-            // $footer.append($claps);
             $footer.append($readMore);
 
             $mediaBody.append($header);
@@ -60,69 +57,3 @@ let gotFeed = response => {
 let extractContent = (html) => {
     return (new DOMParser).parseFromString(html, "text/html").documentElement.textContent;
 }
-
-// var mediumPromise = new Promise(function (resolve) {
-//     var $content = $('#blog-content');
-//     var data = {
-//         rss: 'https://medium.com/feed/@mfeminer'
-//     };
-
-//     $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40mfeminer', data, function (response) {
-//                 if (response.status == 'ok') {
-//                     $("#logo").append(`<img src="${response.feed["image"]}" class="rounded mx-auto d-block">`)
-//                     var display = '';
-//                     $.each(response.items, function (k, item) {
-//                         display += `<div class="card mb-3 mx-auto mr-5 ">`;
-//                         var src = item["thumbnail"]; // use thumbnail url
-//                         display += `<img src="${src}" class="card-img-top" alt="Cover image">`;
-//                         display += `<div class="card-body">`;
-//                         display += `<h5 class="card-title"><a href="${item.link}" target="_blank">${item.title}</a></h5>`;
-//                         var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
-//                         yourString = yourString.replace('h4', 'p');
-//                         yourString = yourString.replace('h3', 'p');
-//                         var maxLength = 120; // maximum number of characters to extract
-//                         //trim the string to the maximum length
-//                         var trimmedString = yourString.substr(0, maxLength);
-//                         //re-trim if we are in the middle of a word
-//                         trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
-//                         display += `<p class="card-text">${trimmedString}...</p>`;
-
-//                         display += `<a href="${item.link}" target="_blank" class="btn btn-outline-success" >Read More</a>`;
-//                         display += '</div></div>';
-//                         return k < 10;
-//                     });
-
-//                     resolve($content.html(display));
-//                 }
-//             });
-//             });
-
-// mediumPromise.then(function()
-//             {
-//                 //Pagination
-//                 pageSize = 4;
-
-//                 var pageCount = $(".card").length / pageSize;
-
-//                 for (var i = 0; i < pageCount; i++) {
-//                     $("#pagin").append(`<li class="page-item"><a class="page-link" href="#">${(i + 1)}</a></li> `);
-//                 }
-//                 $("#pagin li:nth-child(1)").addClass("active");
-//                 showPage = function (page) {
-//                     $(".card").hide();
-//                     $(".card").each(function (n) {
-//                         if (n >= pageSize * (page - 1) && n < pageSize * page)
-//                             $(this).show();
-//                     });
-//                 }
-
-//                 showPage(1);
-
-//                 $("#pagin li").click(function () {
-//                     $("#pagin li").removeClass("active");
-//                     $(this).addClass("active");
-//                     showPage(parseInt($(this).text()))
-//                     return false;
-//                 });
-//             });
-//         });
